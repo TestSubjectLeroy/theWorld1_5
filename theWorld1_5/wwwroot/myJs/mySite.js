f0056d491a1b3fe6ca4cb62f4f58cc721b02d09f
TestSubjectLeroy <the-real@live.com>
2018-03-15 04:04:21 -0400

    initial Commit ASP.NET CORE 1.1  and AngularJs

---
+++ theWorld1_5/wwwroot/myJs/mySite.js
@@ -0,0 +1,23 @@
+﻿
+//mySite.js
+(function () {
+
+
+    //var ele = $("#username");
+    //ele.text("My G.U.D");
+
+    //var main1 = $("#main1");
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show sidebar");
+        }
+        else {
+            $(this).text("hide sidebar");
+        }
+    });
+
+})();
