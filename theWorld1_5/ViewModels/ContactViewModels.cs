7320a07e98cea12382ecc24e51d0629b7e56dcf3
TestSubjectLeroy <the-real@live.com>
2018-03-15 05:27:27 -0400

    add Tag helpers, ModelView for Contact Page.

---
+++ theWorld1_5/ViewModels/ContactViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theWorld1_5.ViewModels
+{
+    public class ContactViewModels
+    {
+        public string name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
