6a0cd3b0aca6db9543fb4a3811d714cdd1a7fb65
TestSubjectLeroy <the-real@live.com>
2018-04-23 02:25:31 -0400

    spinning loading Angular added failed "Directive" atempt

--- theWorld1_5/wwwroot/myJs/tripsController.js
+++ theWorld1_5/wwwroot/myJs/tripsController.js
@@ -9,26 +9,52 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController); 
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [{
-            name: "US trip",
-            created: new Date()
-        },
-            {
-                name: "World trip",
-                created: new Date()
-            }];
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = "";
+
+        
+        vm.isBusy = true;
+        $http.get("/api/trips")
+            .then(function (response) {
+                //Success
+                angular.copy(response.data, vm.trips);
+               
+            }, function (error) {
+                //failure
+                vm.errorMessage = "Failed to load data: " + error;
+              
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            }) ;
+
         vm.addTrip = function () {
-            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
-            vm.newTrip = {};
+
+            vm.isBusy = true;
+            $http.post("/api/trips", vm.newTrip)
+                .then(function (response) {
+                    //Success
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+
+                }, function (error) {
+                    //Fail
+                    vm.errorMessage = "Failed to save data: " + error;
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
 
         };
+
+
     }
      
 })();
